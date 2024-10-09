a651c7d9a5fe383c0e7f69e16e0577c714dfb0d0
pseudoureka <messiah7179@gmail.com>
2024-10-09 23:09:56 +0900

    수정 : 변수, 이벤트핸들러 등록

--- src/index.js
+++ src/index.js
@@ -1,11 +1,37 @@
 import ReactDOM from "react-dom/client";
 
+const WINS = {
+  rock: "scissor",
+  scissor: "paper",
+  paper: "rock",
+};
+
+function getResult(left, right) {
+  if (WINS[left] === right) return "승리";
+  else if (left === WINS[right]) return "패배";
+  return "무승부";
+}
+
+function handleClick() {
+  console.log("가위바위보!");
+}
+
+const me = "rock";
+const other = "scissor";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
     <h1 id="title">가위바위보</h1>
-    <button class="hand">가위</button>
-    <button class="hand">바위</button>
-    <button class="hand">보</button>
+    <h2>{getResult(me, other)}</h2>
+    <button onClick={handleClick} class="hand">
+      가위
+    </button>
+    <button onClick={handleClick} class="hand">
+      바위
+    </button>
+    <button onClick={handleClick} class="hand">
+      보
+    </button>
   </>
 );
