382fc3bb6ee12d9fa6c3fb4fafe85e53771ad8e7
pseudoureka <messiah7179@gmail.com>
2024-10-09 22:40:08 +0900

    1차 수정

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<h1>안녕 리액트!</h1>);
+root.render(
+  <>
+    <h1 id="title">가위바위보</h1>
+    <button class="hand">가위</button>
+    <button class="hand">바위</button>
+    <button class="hand">보</button>
+  </>
+);
