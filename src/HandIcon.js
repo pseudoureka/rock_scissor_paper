ca86749bc74211181e5fc518cebc5f9d16ae0ee1
pseudoureka <messiah7179@gmail.com>
2024-10-10 13:43:02 +0900

    이미지 추가 컴포넌트 개념 학습

---
+++ src/HandIcon.js
@@ -0,0 +1,7 @@
+import Rock from "./assets/rock.svg";
+
+function HandIcon() {
+  return <img src={Rock} alt="rock" />;
+}
+
+export default HandIcon;
--- src/index.js
+++ src/index.js
@@ -1,37 +1,9 @@
 import ReactDOM from "react-dom/client";
-
-const WINS = {
-  rock: "scissor",
-  scissor: "paper",
-  paper: "rock",
-};
-
-function getResult(left, right) {
-  if (WINS[left] === right) return "승리";
-  else if (left === WINS[right]) return "패배";
-  return "무승부";
-}
-
-function handleClick() {
-  console.log("가위바위보!");
-}
-
-const me = "rock";
-const other = "scissor";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
-    <h1 id="title">가위바위보</h1>
-    <h2>{getResult(me, other)}</h2>
-    <button onClick={handleClick} class="hand">
-      가위
-    </button>
-    <button onClick={handleClick} class="hand">
-      바위
-    </button>
-    <button onClick={handleClick} class="hand">
-      보
-    </button>
+    <App />
   </>
 );
