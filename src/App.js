271ecf63ab9274876f0ff2b9203f3201dfb78f20
pseudoureka <messiah7179@gmail.com>
2024-10-12 11:15:56 +0900

    useState를 활용해서 가위바위보 승무패 출력

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
+import { useState } from "react";
 import Button from "./Button";
 import HandButton from "./HandButton";
+import { compareHand, generateRandomHand } from "./utils";
+import HandIcon from "./HandIcon";
+
+function getResult(me, other) {
+  const comparison = compareHand(me, other);
+  if (comparison > 0) return "승리";
+  if (comparison < 0) return "패배";
+  return "무승부";
+}
+
+const INITIAL_VALUE = "rock";
 
 function App() {
-  const handleButtonClick = (value) => console.log(value);
-  const handleClearClick = () => console.log("처음부터");
+  const [hand, setHand] = useState(INITIAL_VALUE);
+  const [otherHand, setOtherHand] = useState(INITIAL_VALUE);
+
+  const handleButtonClick = (nextHand) => {
+    const nextOtherHand = generateRandomHand();
+    setHand(nextHand);
+    setOtherHand(nextOtherHand);
+  };
+  const handleClearClick = () => {
+    setHand(INITIAL_VALUE);
+    setOtherHand(INITIAL_VALUE);
+  };
 
   return (
     <div>
       <Button onClick={handleClearClick}>처음부터</Button>
+      <p>{getResult(hand, otherHand)}</p>
+      <div>
+        <HandIcon value={hand} />
+        VS
+        <HandIcon value={otherHand} />
+      </div>
       <HandButton value="rock" onClick={handleButtonClick} />
       <HandButton value="scissor" onClick={handleButtonClick} />
       <HandButton value="paper" onClick={handleButtonClick} />
